1d81f096a9dc282bfc0d849efa911b0e401c2fd5
ICT ACADEMY <thanvirmuhd@gmail.com>
2023-05-24 16:43:26 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,44 @@
-import React,{useEffect, useState} from 'react'
-import Container from 'react-bootstrap/Container';
-import Table from 'react-bootstrap/Table';
-
-
+import Container from "react-bootstrap/Container";
+import { useForm } from "react-hook-form";
 
 
 export default function App() {
+  
 
 
-const [todo,setTodo] = useState([]);
+  const { register, handleSubmit } = useForm();
+  const onFormSubmit  = data => console.log(data);
+  const onErrors = errors => console.error(errors);
 
 
-const getData = async ()=>{
-const data = await fetch('https://jsonplaceholder.typicode.com/todos');
-const jsonData = await data.json();
-setTodo(jsonData)
+
+
+  return (
+    <div>
+      <Container className="mt-5 pt-5">
+        <h1 className="text-center mb-5">Form</h1>
+
+
+<form onSubmit={handleSubmit(onFormSubmit, onErrors)}>
+  <label htmlFor="">Name</label>
+  <input type="text" name="Name" {...register('Name',{ required: "Name is required" })} id="" /><br></br><br></br>
+  <label htmlFor="">Address</label>
+  <input type="text" name="Address" {...register('Address',{ required: "Address is required" })} id="" /><br></br><br></br>
+<label htmlFor="">Collage</label>
+<input type="text" name="Collage" {...register('Collage',{ required: "Collage is required" })} id="" /><br></br><br></br>
+<label htmlFor="">Phone</label>
+<input type="text" name="Phone" {...register('Phone',{ required: "Phone is required" })} id="" /><br></br><br></br>
+
+  <button type="submit">Submit</button>
+</form>
+
+
+
+
+
+
+      </Container>
+    </div>
+  );
 }
-
-
-
-useEffect(()=>{
-getData();
-},[])
-
-
-
-return (
-<div>
-<Container className='mt-5 pt-5'>
-<h1 className='text-center mb-5'>Todo List</h1>
-<Table striped bordered hover>
-<thead>
-<tr>
-<th>#</th>
-<th>Title</th>
-<th>Completed</th>
-</tr>
-</thead>
-<tbody>
-{
-todo.map((item,index)=>{
-return(
-<tr key={index}>
-<td><input type="checkbox" checked={item.completed} /></td>
-<td>{item.title}</td>
-<td>{item.completed?"completed":"pending"}</td>
-</tr>
-)
-})
-}
-
-</tbody>
-</Table>
-</Container>
-
-
-
-</div>
-)
-}
-
-
 
